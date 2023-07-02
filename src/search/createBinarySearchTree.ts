9d30d0ad5ad41d75387e39b9a2fac6a198fd11d3
JerryP <abrcdf1023@gmail.com>
2023-07-02 17:59:07 +0800

    feat createBinarySearchTree

---
+++ src/search/createBinarySearchTree.ts
@@ -0,0 +1,34 @@
+import TreeNode from './TreeNode'
+
+function insertNode(tree: TreeNode, val: number) {
+  if (!tree.val) {
+    tree.val = val
+    return tree
+  }
+  if (val < tree.val) {
+    if (!tree.left) {
+      tree.left = new TreeNode(val)
+    } else {
+      insertNode(tree.left, val)
+    }
+  } else {
+    if (!tree.right) {
+      tree.right = new TreeNode(val)
+    } else {
+      insertNode(tree.right, val)
+    }
+  }
+}
+
+export default function createBinarySearchTree(arr: (number|null)[]) {
+  const tree = new TreeNode()
+  
+  for (let i = 0; i < arr.length; i++) {
+    const el = arr[i];
+    if (el) {
+      insertNode(tree, el)
+    }
+  }
+
+  return tree
+}
